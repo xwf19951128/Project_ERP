9ab5504bf46c8694910561f05999e4b0c1bcd66a
xwf19951128 <872814505@qq.com>
2019-08-08 16:39:58 +0800

    add dev

--- src/main/java/com/cskaoyan/controller/TechnologyController.java
+++
@@ -1,9 +0,0 @@
-package com.cskaoyan.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/technology")
-public class TechnologyController {
-//    @RequestMapping("/find")
-//    public
-}
