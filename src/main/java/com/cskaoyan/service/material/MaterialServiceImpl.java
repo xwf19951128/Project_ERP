89c924ab2284bc10d290d6ff97ba0ad18666377f
tangxuan20141402 <912841857@qq.com>
2019-08-09 20:32:21 +0800

    tx-"物料信息的CRUD+模糊查询"

--- src/main/java/com/cskaoyan/service/material/MaterialServiceImpl.java
+++ src/main/java/com/cskaoyan/service/material/MaterialServiceImpl.java
@@ -30,4 +30,52 @@ public class MaterialServiceImpl implements MaterialService {
     public int countMaterialCount() {
         return materialMapper.countMaterialCount();
     }
+
+    @Override
+    public int insertMaterial(Material material) {
+        return materialMapper.insertMaterial(material);
+    }
+
+    @Override
+    public int updateNoteById(Material material) {
+        return materialMapper.updateNoteById(material);
+    }
+
+    @Override
+    public int updateAllById(Material material) {
+        return materialMapper.updateAllById(material);
+    }
+
+    @Override
+    public int deleteMaterialsByIds(String[] ids) {
+        return materialMapper.deleteMaterialsByIds(ids);
+    }
+
+    @Override
+    public int countSearchMaterialCountById(String searchValue) {
+        String like = "%" + searchValue + "%";
+        return materialMapper.countSearchMaterialCountById(like);
+    }
+
+    @Override
+    public List<Material> listPageSearchMaterialsById(int page, int rows, String searchValue) {
+        int limit = rows;
+        int offset = (page-1)*rows;
+        String like = "%" + searchValue + "%";
+        return materialMapper.listPageSearchMaterialsById(limit, offset, like);
+    }
+
+    @Override
+    public int countSearchMaterialCountByType(String searchValue) {
+        String like = "%" + searchValue + "%";
+        return materialMapper.countSearchMaterialCountByType(like);
+    }
+
+    @Override
+    public List<Material> listPageSearchMaterialsByType(int page, int rows, String searchValue) {
+        int limit = rows;
+        int offset = (page-1)*rows;
+        String like = "%" + searchValue + "%";
+        return materialMapper.listPageSearchMaterialsByType(limit, offset, like);
+    }
 }
