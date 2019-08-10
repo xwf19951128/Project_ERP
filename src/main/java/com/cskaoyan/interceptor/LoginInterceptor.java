002397051c90e24181c0a688af09b423a9c8ed26
liza8023 <@126.com>
2019-08-10 17:34:03 +0800

    add loginInterceptor & logout

---
+++ src/main/java/com/cskaoyan/interceptor/LoginInterceptor.java
@@ -0,0 +1,22 @@
+package com.cskaoyan.interceptor;
+
+import com.cskaoyan.bean.login.SysUser;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        if (requestURI.contains("ajaxLogin")) {
+            return true;
+        }
+        SysUser activeUser = (SysUser) request.getSession().getAttribute("activeUser");
+        if (activeUser == null) {
+            request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request,response);
+        }
+        return true;
+    }
+}
