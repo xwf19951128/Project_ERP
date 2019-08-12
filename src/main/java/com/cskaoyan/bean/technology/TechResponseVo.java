335f3bd98826447b028a9af30838f4019fff0ff5
xwf19951128 <872814505@qq.com>
2019-08-12 11:42:30 +0800

    hibernate参数校验

---
+++ src/main/java/com/cskaoyan/bean/technology/TechResponseVo.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.bean.technology;
+
+public class TechResponseVo<T> {
+    T data;
+    int status;
+    String msg;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
